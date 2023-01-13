efb2491ca26d8481d81d1b91c1d07a282f11ad90
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-13 23:31:44 +0300

    Using functions as props to pass data up and down child/parent r/ship.

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColour, color }) {
+  console.log(onChangeColour);
+
+  // function to invoke onChangeColour and pass random color as argument. onClick takes in that new function
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColour(newColor);
+  }
+  return <div 
+            className="child" 
+            // using the color props passed down from Parent
+            style={{ backgroundColor: color }} 
+            onClick={handleClick}
+          />;
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -4,12 +4,23 @@ import Child from "./Child";
 
 function Parent() {
   const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+  const [color, setColor] = useState(randomColor); // initial value for color state
+
+  // store color of the Children in the Parent state
+  const [childrenColor, setChildrenColor] = useState('#FFF');
+
+  // newChildColor argument to update Child, and send data back to Parent
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor); // update color state to a new value
+    setChildrenColor(newChildColor);
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      {/* data of the Child color passed down as props */}
+      <Child color={childrenColor} onChangeColour={handleChangeColor}/>
+      <Child color={childrenColor} onChangeColour={handleChangeColor}/>
     </div>
   );
 }
